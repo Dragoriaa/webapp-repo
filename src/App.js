4e1ae1c42877b7a1fef6897ab999a493fa069708
DELBREIL <delbreilclem@gmail.com>
2024-12-05 20:29:38 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [user, setUser] = useState(null);
+  
+  Auth.currentAuthenticatedUser()
+    .then(setUser)
+    .catch(() => setUser(null));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React with AWS Amplify</h1>
+      {user ? <p>Hello, {user.username}</p> : <p>Please sign in</p>}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
